6875dee6e9171a6d9a4a06443f802b739e559d5c
aprotonics <48241333+aprotonics@users.noreply.github.com>
2020-06-18 11:59:16 +0500

    Add files via upload

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ let todoNewItem = document.querySelector('.todo-new-item');
 let todoAddButton = document.querySelector('.todo-add-button');
 let todoItems = document.querySelectorAll('.todo-item');  
 let todoHeadItemText = document.querySelector('.todo-headitem__text');
+todoAddButton.disabled = true;
 
 // Adding item to list
 function addItem() {
@@ -26,49 +27,18 @@ function addItem() {
     todoList.appendChild(todoItem);
 
     todoNewItem.value = '';
+    todoAddButton.disabled = true;
 }
 
-// Cross out selected item
+// Crossing out selected item
 function getFulfilled() {
-    todoItems = document.querySelectorAll('.todo-item');  
-    for (let i = 0; i < todoItems.length; i++) {
-        let todoItem = todoItems[i];
-        let todoItemBox = todoItem.querySelector('.todo-item__box');
-        let todoItemText = todoItem.querySelector('.todo-item__text');
-        let todoItemDelete = todoItem.querySelector('.todo-item__item-delete');
-
-        // Event Listener for decorating
-        todoItemBox.addEventListener('change', function() {
-            if (todoItemText.style.textDecoration !== 'line-through') {
-                todoItemText.style.textDecoration = 'line-through';
-                todoItemDelete.classList.add('todo-item__item-delete_show');
-            } else {
-                todoItemText.style.textDecoration = 'none';    
-                todoItemDelete.classList.remove('todo-item__item-delete_show');
-            }
-        });
-
-        // Event Listener for deleting
-        todoItemDelete.addEventListener('click', function() {
-            todoItem.remove();
-        });
-    }
-}
-
-
-// Event Listener for adding
-todoAddButton.addEventListener('click', function(evt) {
-    evt.preventDefault();
-    addItem(); 
-    
-    // Add Event Listener to new Items on change of checkbox 
-    todoItems = document.querySelectorAll('.todo-item');
+    let todoItems = document.querySelectorAll('.todo-item');
     let todoItem = todoItems[todoItems.length - 1];
     let todoItemBox = todoItem.querySelector('.todo-item__box');
     let todoItemText = todoItem.querySelector('.todo-item__text');
     let todoItemDelete = todoItem.querySelector('.todo-item__item-delete');
 
-    // Event Listener for decorating
+    // Event Listener for crossing out
     todoItemBox.addEventListener('change', function() {
         if (todoItemText.style.textDecoration !== 'line-through') {
             todoItemText.style.textDecoration = 'line-through';
@@ -78,36 +48,92 @@ todoAddButton.addEventListener('click', function(evt) {
             todoItemDelete.classList.remove('todo-item__item-delete_show');  
         }    
     });
+}
 
-    // Event Listener for deleting
+// Deleting item from list
+function getDeleted() {
+    let todoItems = document.querySelectorAll('.todo-item');
+    let todoItem = todoItems[todoItems.length - 1];
+    let todoItemDelete = todoItem.querySelector('.todo-item__item-delete');
+
+    // Event Listener for deleting item
     todoItemDelete.addEventListener('click', function() {
         todoItem.remove();
     });
+}
+
+// Getting sorted item list
+function getSorted() {
+    let sortedValue = 0;
+    
+    // Event Listener for sorting items
+    todoHeadItemText.addEventListener('click', function() {
+
+        // Getting sorted array of item names
+        let todoItems = document.querySelectorAll('.todo-item');
+        console.log(todoItems);
+        let itemNames = [];
+        for (let i = 0; i < todoItems.length; i++) {
+            let todoItemText = todoItems[i].querySelector('.todo-item__text');
+            let itemName = todoItemText.textContent;
+            itemNames.push(itemName);
+        }
+        switch (sortedValue) {
+            case 0:
+                itemNames.sort((a, b) => a > b ? 1 : -1);
+                break;
+
+            case 1:
+                itemNames.sort((a, b) => a > b ? -1 : 1);
+                break;
+
+            case -1:
+                itemNames.sort((a, b) => a > b ? 1 : -1);
+                break;
+        }
+            
+        // Adding sorted item names to list
+        for (let i = 0; i < itemNames.length; i++) {
+            let todoItemText = todoItems[i].querySelector('.todo-item__text');
+            todoItemText.textContent = itemNames[i];
+        }
+
+        switch (sortedValue) {
+            case 0:
+                sortedValue = 1;
+                break;
+
+            case 1:
+                sortedValue = -1;
+                break;
+
+            case -1:
+                sortedValue = 1;
+                break;
+        }
+    });
+}
+
+// Event Listener for changing input text
+todoNewItem.addEventListener('input', function() {
+    if (!todoNewItem.value) {
+        todoAddButton.disabled = true;
+    } else {
+        todoAddButton.disabled = false;
+    }
+})
+
+// Event Listener for adding item
+todoAddButton.addEventListener('click', function(evt) {
+    evt.preventDefault();
+    addItem(); 
+    getFulfilled();
+    getDeleted();
 });
 
-getFulfilled();
+// Getting sorted item list
+getSorted();
 
 
 
-// Event Listener for sorting
-todoHeadItemText.addEventListener('click', function() {
-    // Getting sorted array of item names
-    todoItems = document.querySelectorAll('.todo-item');
-    console.log(todoItems);
-    let itemNames = [];
-    for (let i = 0; i < todoItems.length; i++) {
-        let todoItemText = todoItems[i].querySelector('.todo-item__text');
-        let itemName = todoItemText.textContent;
-        itemNames.push(itemName);
-    }
-    console.log(itemNames);
-    itemNames.sort();
-    console.log(itemNames);
 
-    // Adding sorted item names to list
-    for (let i = 0; i < itemNames.length; i++) {
-        let todoItemText = todoItems[i].querySelector('.todo-item__text');
-        todoItemText.textContent = itemNames[i];
-    }
-    console.log(todoItems);
-});
